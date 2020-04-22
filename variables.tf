b437526233a5df90cf901defbc981948b3468f9e
greyhoundforty <ryantiffany@fastmail.com>
2020-04-22 15:37:15 -0500

    Updated variables
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- variables.tf
+++ variables.tf
@@ -1,25 +1,30 @@
 variable "datacenter" {
-  type = string
-  default = "sjc04"
+  description = "The Datacenter where the instance will be deployed."
+  type        = string
+  default     = "sjc04"
 }
 
 variable "node_count" {
-  type    = string
-  default = "1"
+  description = "The number of instances to deploy."
+  type        = string
+  default     = "1"
 }
 
 variable "os_reference_code" {
-  type = string
-  default = "UBUNTU_18_64"
+  description = "The Operating System for the instance."
+  type        = string
+  default     = "UBUNTU_18_64"
 }
 
-variable "flavor_key_name" {
-  type = string
-  default = "BL2_2X4X100"
+variable "flavor" {
+  description = "The size / flavor of the instance."
+  type        = string
+  default     = "BL2_2X4X100"
 }
 
 variable "domain" {
-  type    = string
-  default = "cdetesting.com"
+  description = "Domain name for instance."
+  type        = string
+  default     = "cdetesting.com"
 }
 
