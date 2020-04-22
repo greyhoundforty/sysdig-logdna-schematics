d05b1494c3649ab7445cb55984b49ad175b72e05
greyhoundforty <ryantiffany@fastmail.com>
2020-04-22 15:20:23 -0500

    Removed maps for Schematics
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ resource "ibm_compute_vm_instance" "node" {
   count                = var.node_count
   hostname             = "rt-test-${count.index + 1}"
   domain               = var.domain
-  os_reference_code    = var.os_reference_code["u18"]
-  datacenter           = var.datacenter["us-east2"]
+  os_reference_code    = var.os_reference_code
+  datacenter           = var.datacenter
   network_speed        = 1000
   hourly_billing       = true
   private_network_only = false
   local_disk           = true
   user_metadata        = file("install.yml")
-  flavor_key_name      = var.flavor_key_name["blocal-medium"]
-  tags                 = ["ryantiffany", var.datacenter["us-east2"], "sysdig-logdna-alert-test"]
+  flavor_key_name      = var.flavor
+  tags                 = ["ryantiffany", var.datacenter, "sysdig-logdna-alert-test"]
   ssh_key_ids          = [data.ibm_compute_ssh_key.deploymentKey.id]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,26 +1,6 @@
 variable "datacenter" {
-  type = map(string)
-
-  default = {
-    us-south1 = "dal10"
-    us-south2 = "dal12"
-    us-south3 = "dal13"
-    us-east1  = "wdc04"
-    us-east2  = "wdc06"
-    us-east3  = "wdc07"
-    eu-de1    = "fra02"
-    eu-de2    = "fra04"
-    eu-de3    = "fra05"
-    eu-lon1   = "lon04"
-    eu-lon2   = "lon06"
-    eu-lon3   = "lon05"
-    apac1     = "tok02"
-    apac2     = "tok04"
-    apac3     = "tok05"
-    aus1      = "syd01"
-    aus2      = "syd04"
-    aus3      = "syd05"
-  }
+  type = string
+  default = "sjc04"
 }
 
 variable "node_count" {
@@ -29,33 +9,13 @@ variable "node_count" {
 }
 
 variable "os_reference_code" {
-  type = map(string)
-
-  default = {
-    u16     = "UBUNTU_16_64"
-    u18     = "UBUNTU_18_64"
-    rhel6   = "REDHAT_6_64"
-    rhel7   = "REDHAT_7_64"
-    cent6   = "CENTOS_6_64"
-    cent7   = "CENTOS_7_64"
-    deb8    = "DEBIAN_8_64"
-    deb9    = "DEBIAN_9_64"
-    win2012 = "WIN_2012-STD_64"
-    win2016 = "WIN_2016-STD_64"
-    win2019 = "WIN_2019-STD_64"
-  }
+  type = string
+  default = "UBUNTU_18_64"
 }
 
 variable "flavor_key_name" {
-  type = map(string)
-  default = {
-    bsan-small    = "B1_2X4X100"
-    bsan-medium   = "B1_8X16X100"
-    bsan-large    = "B1_16X32X100"
-    blocal-small  = "BL2_2X4X100"
-    blocal-medium = "BL2_8X16X100"
-    blocal-large  = "BL2_16X32X100"
-  }
+  type = string
+  default = "BL2_2X4X100"
 }
 
 variable "domain" {
